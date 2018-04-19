50b7c0acc256d92a138a0c714df4b33312479ebb
Sven Heinen <sven.heinen@qnh.nl>
2018-04-19 15:16:24 +0200

    Do something radical

--- site/profile/manifests/icinga_satellite.pp
+++ site/profile/manifests/icinga_satellite.pp
@@ -1,10 +1,14 @@
-class profile::icinga_satellite {
-
+class profile::icinga_satellite(
+  $slave_zone,
+  $parent_endpoints,
+  $parent_zone = 'master',
+  $slave_ip = $::ipaddress,
+) {
   class { '::icinga2':
     manage_repo => true,
     confd     => true,
     constants => {
-      'ZoneName' => 'satellite',
+      'ZoneName' => $slave_zone,
     },
   }
 
@@ -18,17 +22,56 @@ class profile::icinga_satellite {
       }
     },
     zones           => {
-      'master' => {
-        'endpoints' => ['puppet'],
-      },
-      'satellite' => {
+      'ZoneName' => {
         'endpoints' => ['icingaslave.qnh.local'],
-        'parent'    => 'master'
+        'parent'    => $parent_zone,
       }
     }
   }
 
-  icinga2::object::zone { 'global-templates':
-    global => true,
+  ::icinga2::object::endpoint { $parent_endpoints: }
+
+  ::icinga2::object::zone { $parent_zone:
+    endpoints => $parent_endpoints,
   }
+
+  ::icinga2::object::zone { ['global-templates', 'windows-commands', 'linux-commands']:
+    global => true,
+    order  => '47',
+  }
+
+  @@::icinga2::object::endpoint { $::fqdn:
+    host => $slave_ip,
+    tag  => "icinga2::parent::${parent_zone}",
+  }
+
+  @@::icinga2::object::zone { $slave_zone:
+    endpoints => [ $::fqdn ],
+    parent    => $parent_zone,
+    tag       => "icinga2::parent::${parent_zone}",
+  }
+
+  @@file { "/etc/icinga2/zones.d/${slave_zone}":
+    ensure => directory,
+    owner  => 'icinga',
+    group  => 'icinga',
+    mode   => '0750',
+    tag    => 'icinga2::slave::zone',
+  }
+
+  ::Icinga2::Object::Endpoint <<| tag == "icinga2::parent::${slave_zone}" |>>
+  ::Icinga2::Object::Zone <<| tag == "icinga2::parent::${slave_zone}" |>>
+
+  @@::icinga2::object::host { $::fqdn:
+    # Puppet 4 syntax
+    * => deep_merge({
+      display_name => $::hostname,
+      address      => $slave_ip,
+      target       => "/etc/icinga2/zones.d/${slave_zone}/${::hostname}.conf",
+      zone         => $parent_zone,
+      vars         => {
+        'cluster_zone' => $slave_zone,
+      },
+    }, hiera_hash(icinga2::host)),
+}
 }
